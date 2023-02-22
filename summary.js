84818d8b13937a8533ad63f25b99a65720ac8ff0
johirjoy <joy.sd69@gmail.com>
2023-02-23 02:19:08 +0600

    first commit

---
+++ summary.js
@@ -0,0 +1,17 @@
+// 1. optional chaining
+// student.marks?.math
+
+// must
+// 2. map, filter, find
+
+// 3. (optional): forEach, reduce (advanced)
+
+// 4. class
+
+// 5. (optional) inheritance, super
+
+// 6. (must) prototypical Inheritance
+
+const nums = [1,2,3,4,5];
+let output = nums.filter(n => n%2);
+console.log(output);
